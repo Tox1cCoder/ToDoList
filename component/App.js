001b8762fb1727462c0adc5a313f21b5775273ed
Tox1cCoder <hroryu.nguyen@gmail.com>
2023-10-18 13:51:19 +0700

    Initialize

--- component/App.js
+++ component/App.js
@@ -2,7 +2,10 @@ import html from '../core.js'
 
 function App() {
     return html`
-        <h1>Hello World</h1>
+    <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo" placeholder="What needs to be done?" autofocus>
+    </header>
     `
 }
 
